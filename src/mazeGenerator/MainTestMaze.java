9e644f30d7a41aa42e189ae016c0277754f85e41
Andrew Rizk <97995173+AndrewidRizk@users.noreply.github.com>
2024-04-09 13:15:34 -0400

    done

--- src/mazeGenerator/MainTestMaze.java
+++ src/mazeGenerator/MainTestMaze.java
@@ -1,26 +1,26 @@
-package PE1;
+package mazeGenerator;
 
 @SuppressWarnings("javadoc")
 public class MainTestMaze {
 
 	public static void main(String[] args) {
-		String [][] array = {{"1110", "1010", "1010", "1000", "1010", "1011"},
-							{"1110", "1000", "1001", "0101", "1100", "1001"},
-							{"1100", "0011", "0101", "0110", "0011", "0101"},
-							{"0101", "1101", "0110", "1001", "1110", "0001"},
-							{"0110", "0011", "1100", "0010", "1010", "0011"}};
-			
+		String[][] array = {
+				{ "1110", "1010", "1010", "1000", "1010", "1011" },
+				{ "1110", "1000", "1001", "0101", "1100", "1001" },
+				{ "1100", "0011", "0101", "0110", "0011", "0101" },
+				{ "0101", "1101", "0110", "1001", "1110", "0001" },
+				{ "0110", "0011", "1100", "0010", "1010", "0011" } };
+
 		MazeSolver a = new MazeSolver();
 		a.setup(array);
 		Maze b = new Maze(array);
-		
+
 		b.getReference().toString();
-		System.out.println(a.ifGate(1, 5));
+		System.out.println("----------------------------");
 		String pass = a.findPath(4, 1);
 		System.out.println(pass);
 		System.out.println("----------------------------");
-		
-		
+
 	}
 
 }
