f45a06d73465c3a7c3f713fc38aabf5e49ae294a
Andrew Rizk <97995173+AndrewidRizk@users.noreply.github.com>
2024-04-09 13:48:46 -0400

    adding mvn

---
+++ MazeSolver/README.md
@@ -0,0 +1,2 @@
+# Maze-Solver
+A maze solver algorism using recursion and OOP in java
---
+++ MazeSolver/src/test/java/com/example/MazeSolver/MazeSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MazeSolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazeSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
